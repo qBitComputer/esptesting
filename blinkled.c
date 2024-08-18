5d54168220373d53840eb48c2a85b33ec37f753b
Quinten <56535364+qBitComputer@users.noreply.github.com>
2024-08-18 14:59:24 +0200

    maybe it will work now?

--- blinkled.c
+++ blinkled.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
 void app_main(void)
 {
 	char *yesTask = pcTaskGetName(NULL);
-	ESPLOG(yesTask, "Starting up!");
+	ESPLOGI(yesTask, "Starting up!");
 	while (1){
 		;;
 	} 
