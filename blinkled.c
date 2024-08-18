4b7a23f0d9fcfb1ada0d1b1526c1216becc9ef0b
Quinten <56535364+qBitComputer@users.noreply.github.com>
2024-08-18 15:25:51 +0200

    made this blinky sketch

--- blinkled.c
+++ blinkled.c
@@ -2,12 +2,19 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
+#include "gpio_driver.h"
+
 void app_main(void)
 {
 	char *yesTask = pcTaskGetName(NULL);
 	ESP_LOGI(yesTask, "Starting up!");
+	gpio_reset_pin(26);
+	gpio_set_direction(26, GPIO_MODE_OUTPUT);
 	while (1){
-
+		gpio_set_level(26, 1);
 		vTaskDelay(1000);
+		gpio_set_level(26, 0);
+		vTaskDelay(1000);
+
 	} 
 }
