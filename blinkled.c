ef1ac7b01f04f7ba3b1233f878d0e217b577e4dc
Quinten <56535364+qBitComputer@users.noreply.github.com>
2024-08-18 15:54:07 +0200

    added a 3rd led and made it a traffic light

--- blinkled.c
+++ blinkled.c
@@ -12,13 +12,18 @@ void app_main(void)
 	gpio_set_direction(26, GPIO_MODE_OUTPUT);
 	gpio_reset_pin(27);
 	gpio_set_direction(27, GPIO_MODE_OUTPUT);
+	gpio_reset_pin(25);
+	gpio_set_direction(25, GPIO_MODE_OUTPUT);
 	while (1){
-		gpio_set_level(26, 1);
-		vTaskDelay(1000 / portTICK_PERIOD_MS);
-		gpio_set_level(26, 0);
 		gpio_set_level(27, 1);
-		vTaskDelay(1000 / portTICK_PERIOD_MS);
+		vTaskDelay(2000 / portTICK_PERIOD_MS);
 		gpio_set_level(27, 0);
+		gpio_set_level(25, 1);
+		vTaskDelay(2000 / portTICK_PERIOD_MS);
+		gpio_set_level(25, 0);
+		gpio_set_level(26, 1);
+		vTaskDelay(2000 / portTICK_PERIOD_MS);
+		gpio_set_level(26, 0);
 
 
 	} 
